6833aea0765ef8f4358a6210a66e431653ac6515
BasmaKuhail <basmakuhail2003@gmail.com>
2026-02-10 16:51:21 +0200

    languge switch setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,32 @@
-export default function index() {
-    return (
-        <h1>Home</h1>
-    )
+import { useTranslation } from 'next-i18next';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import { useRouter } from 'next/router';
+
+export async function getStaticProps({ locale }: { locale: string }) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ['common'])), // load 'common' namespace
+    },
+  };
+}
+
+export default function Index() {
+  const { t } = useTranslation('common');
+  const router = useRouter();
+  const { locale, pathname, asPath, query } = router;
+
+  const nextLocale = locale === 'en' ? 'ar' : 'en';
+
+  return (
+    <>
+      <h1>{t('welcome')}</h1>
+      <button
+        onClick={() =>
+          router.push({ pathname, query }, asPath, { locale: nextLocale })
+        }
+      >
+        Switch to {nextLocale}
+      </button>
+    </>
+  );
 }
