3a34c71a4f214f9467da43ca6a7ec32d191bef0a
BasmaKuhail <basmakuhail2003@gmail.com>
2026-02-22 21:00:34 +0200

    enhance proficiency in validating the form

--- components/register/input.tsx
+++ components/register/input.tsx
@@ -7,10 +7,10 @@ type InputProps = {
     inputText: string;
     value: string;
     onChange: (value: string) => void;
+    isTrue: boolean;
 }
 
-export default function Input({label, type, value, onChange}: InputProps) {
-    const [inputValue, setInputValue] = useState<string>('');
+export default function Input({label, type, value, onChange, isTrue}: InputProps) {
     const [passSrc, setPassSrc] = useState<string>('/icons/unshowPass.svg');
     const [inputType, setInputType] = useState<'text' | 'email' | 'password'>(type);
     const togglePasswordVisibility = () => {
@@ -18,44 +18,21 @@ export default function Input({label, type, value, onChange}: InputProps) {
         setPassSrc((prev) => (prev === '/icons/unshowPass.svg' ? '/icons/showPass.svg' : '/icons/unshowPass.svg'));
     };
 
-    const [validEmail, setValidEmail] = useState<boolean>(true);
-    const checkEmail = (email: string) => {
-        if(!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(email)) {
-            setValidEmail(false);
-        } else {
-            setValidEmail(true);
-        }
-    }
 
-    const [validPassword, setValidPassword] = useState<boolean>(true);
-    const checkPassword = (password: string) => {
-        if(password.length < 8 || !/[A-Z]/.test(password) || !/[a-z]/.test(password) || !/[0-9]/.test(password) || !/[@$!%*?&]/.test(password)) {
-            setValidPassword(false);
-        } else {
-            setValidPassword(true);
-        }
-    }
 
     return (
         <div className="flex flex-col gap-2">
             <label className="text-sm font-bold text-right">{label}</label>
             <div className="relative">
                 <input 
-                    onChange={(e) => {onChange(e.target.value); checkEmail(e.target.value); checkPassword(e.target.value); setInputValue(e.target.value)}} 
+                    onChange={(e) => {onChange(e.target.value)} }
                     type={inputType} 
-                    value={inputValue}
+                    value={value}
                     placeholder={label}
                     className={`border rounded-inpt p-2 w-full text-right focus:outline-none text-inpt h-[52px] md:h-[45px]
                         ${
-                        type === 'email' && inputValue
-                            ? validEmail
-                            ? 'border-[#1A71F6]'
-                            : 'border-[#FF1F1F]'
-                            : ''
-                        }
-                        ${
-                        type === 'password' && inputValue
-                            ? validPassword
+                        value
+                            ? isTrue
                             ? 'border-[#1A71F6]'
                             : 'border-[#FF1F1F]'
                             : ''
