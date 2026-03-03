a7c6bdee70b1c2de9f5abccf84e46b59cd086722
BasmaKuhail <basmakuhail2003@gmail.com>
2026-03-03 19:18:47 +0200

    update redirecting for authed user

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -1,18 +1,25 @@
 import Form from "@/components/register/form";
 import { useRouter } from "next/router";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function Login() {
     //re direct to home page if user is already logged in
     const router = useRouter();
-    useEffect(() => {
-    const token = localStorage.getItem("token");
+    const [checkingAuth, setCheckingAuth] = useState(true);
 
-    if (token) {
-      router.push("/");
-    }
-  }, []);
+    useEffect(() => {
+        const token = localStorage.getItem("token");
+
+        if (token) {
+        router.replace("/");
+        } else {
+        setCheckingAuth(false);
+        }
+    }, []);
   
+    if (checkingAuth) {
+        return null; // or loading spinner
+    }
     return (
         <Form isRegister={false} />
     )
