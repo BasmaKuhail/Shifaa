e6ffd718f1ae9270af42d541bebd5ac39b1bdc6c
BasmaKuhail <basmakuhail2003@gmail.com>
2026-05-03 22:34:36 +0300

    small fixes

--- components/home/HomePage.tsx
+++ components/home/HomePage.tsx
@@ -91,7 +91,7 @@ export default function Home (){
                 <div className="absolute inset-0 z-10 pt-[75px] hidden lg:block">
                     <div className="px-4 md:px-8 lg:px-20 xl:px-30 pt-4"><SecondaryHeader/></div>
                     
-                    <div className='mt-20'>
+                    <div id="search" className='mt-20'>
                         <SearchHome />
                     </div>
                 </div>
@@ -99,7 +99,7 @@ export default function Home (){
                 <div className="absolute inset-0 -top-full block lg:hidden w-full flex items-center justify-center">
                     <MobileHeader/>
                 </div>
-                <div className='-mt-10 block lg:hidden mb-40'>
+                <div id="search" className='-mt-10 block lg:hidden mb-40'>
                     <SearchHome />
                 </div>
             </div>
