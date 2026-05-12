72802d7d74424c8337f30c15385c858a4cdee543
BasmaKuhail <basmakuhail2003@gmail.com>
2026-05-12 22:37:14 +0300

    build invitaion section UI

---
+++ pages/invitations/index.tsx
@@ -0,0 +1,10 @@
+import Invitations from "@/components/dashboard/invitations/Invitations";
+import DashboardLayout from "@/layouts/DashboardLayout";
+
+export default function InvitationsIndex() {
+    return(
+        <DashboardLayout>
+            <Invitations/>
+        </DashboardLayout>
+    )
+}
