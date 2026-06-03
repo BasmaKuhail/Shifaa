a17dc7c6ef1734fa7034a526957e4574245c7e90
BasmaKuhail <basmakuhail2003@gmail.com>
2026-06-03 22:47:17 +0300

    send attachments to api

--- pages/403.tsx
+++ pages/403.tsx
@@ -4,27 +4,7 @@ import error403 from "@/public/images/errors/error403.png"
 
 export default function ForbiddenPage() {
   return (
-    <ErrorContatiner image={error403} error={403} message="عذراً، ليس لديك صلاحية للوصول لهذه الصفحة"/>
-    // <main className="min-h-screen flex items-center justify-center px-4">
-      
-    //   <div className="max-w-md text-center space-y-4">
-    //     <h1 className="text-3xl font-bold text-black-400">403</h1>
+    <ErrorContatiner image={error403} title="نأسف!" message="الصفحة التي تحاول الوصول إليها غير متاحة لكم"/>
 
-    //     <p className="text-lg font-semibold text-black-400">
-    //       ليس لديك صلاحية للوصول لهذه الصفحة
-    //     </p>
-
-    //     <p className="text-sm text-black-300">
-    //       يبدو أن حسابك لا يملك الصلاحيات المطلوبة لفتح هذا القسم.
-    //     </p>
-
-    //     <Link
-    //       href="/"
-    //       className="inline-block rounded-lg bg-blue-500 px-6 py-2 font-semibold text-white"
-    //     >
-    //       العودة للرئيسية
-    //     </Link>
-    //   </div>
-    // </main>
   );
 }
