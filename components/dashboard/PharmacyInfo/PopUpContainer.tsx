5a27d929230921bd75cd903478eac0727c35af16
BasmaKuhail <basmakuhail2003@gmail.com>
2026-06-19 21:15:57 +0300

    mobile popup

--- components/dashboard/PharmacyInfo/PopUpContainer.tsx
+++ components/dashboard/PharmacyInfo/PopUpContainer.tsx
@@ -3,7 +3,7 @@ import { ReactNode } from "react";
 export default function PopupContainer({children, onClose}:{children: ReactNode, onClose: () => void}){
     return(
         <div dir="rtl" className="fixed inset-0 z-[9999] flex items-center justify-center bg-black/40 px-4" onClick={onClose}>
-            <div className="bg-white w-[50%] rounded-[14px] p-5 px-10 pb-10 flex flex-col items-center justify-center gap-5" onClick={(e) => e.stopPropagation()}>
+            <div className="bg-white w-[90%] md:w-[70%] lg:w-[50%] rounded-[14px] p-5 px-10 pb-10 flex flex-col items-center justify-center gap-5" onClick={(e) => e.stopPropagation()}>
                 {children}
             </div>
         </div>
