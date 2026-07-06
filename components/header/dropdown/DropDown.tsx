65203c09664bf117b667fad62d6451d47f055fdb
BasmaKuhail <basmakuhail2003@gmail.com>
2026-07-06 23:44:41 +0300

    AI chat bot

--- components/header/dropdown/DropDown.tsx
+++ components/header/dropdown/DropDown.tsx
@@ -1,30 +1,33 @@
 
 import { Dispatch, useState } from 'react';
-import { logout as logoutService } from '@/services/auth';
-import { useRouter } from 'next/router';
-import ProfileIcon from '../../ProfileIcon';
 import { User } from '@/types/UserType';
 import Container from './Container';
 import Settings from './Settings';
 import MainMenu from './MainMenu';
+import MedicalChatPanel from './MedicalChatPanel';
 type headerProps = {
     user: User
     profileOpened:boolean,
     setProfileOpened:Dispatch<React.SetStateAction<boolean>>,
 }
 
-export default function HeaderDropDown({user, profileOpened, setProfileOpened}:headerProps){
+export default function HeaderDropDown({user}:headerProps){
     
     const [isSettingsOpen, setIsSettingsOpen] = useState(false)
+    const [isChatOpen, setIsChatOpen] = useState(false)
     
     const contentToShow = () => {
-        if(isSettingsOpen){
+        if(isChatOpen){
+            return(
+                <MedicalChatPanel onClose={() => setIsChatOpen(false)}/>
+            )
+        }else if(isSettingsOpen){
             return(
                 <Settings/>
             )
         }else{
             return(
-                <MainMenu setIsSettingsOpen={setIsSettingsOpen} user={user} profileOpened={profileOpened} setProfileOpened={setProfileOpened}/>
+                <MainMenu setIsChatOpen={setIsChatOpen} setIsSettingsOpen={setIsSettingsOpen} user={user}/>
             )
         }
     }
